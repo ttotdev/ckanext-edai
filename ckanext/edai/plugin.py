e437129ac6f5c94e1c6492d2b4a8c4f55878d8e0
Tibor <you@example.com>
2020-08-19 12:35:06 -0400

    Properly using css resource. Yessie's change of front page (Machine Learning).

--- ckanext/edai/plugin.py
+++ ckanext/edai/plugin.py
@@ -52,7 +52,15 @@ class EdaiPlugin(plugins.SingletonPlugin):
 
         # Add this plugin's templates dir to CKAN's extra_template_paths, so
         # that CKAN will use this plugin's custom templates.
-        # 'templates' is the path to the templates dir, relative to this
-        # plugin.py file.
-        toolkit.add_public_directory(config, 'public')
         toolkit.add_template_directory(config, 'templates')
+
+        # Add this plugin's public dir to CKAN's extra_public_paths, so
+        # that CKAN will use this plugin's custom static files.
+        toolkit.add_public_directory(config, 'public')
+
+        # Register this plugin's fanstatic directory with CKAN.
+        # Here, 'fanstatic' is the path to the fanstatic directory
+        # (relative to this plugin.py file), and 'example_theme' is the name
+        # that we'll use to refer to this fanstatic directory from CKAN
+        # templates.
+        toolkit.add_resource('fanstatic', 'edai_theme')
